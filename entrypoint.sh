d7b01f629fc14d7af6bcdc72f17706edbb1b654a
MJ <john.hervicc@gmail.com>
2022-01-07 02:53:02 +0100

    (#1) Improve logging in entrypoint script.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,12 @@
 set -m
 
 echo "Running wmllint."
-lint=$(python data/tools/wmllint -d . 2>&1 | tee /dev/stdout)
+lint=$(python data/tools/wmllint -d . 2>&1)
+printf "%s\n" "${lint[@]}" | tac
+
 echo "Running wmlindent."
-indent=$(python data/tools/wmlindent -d . 2>&1 | tee /dev/stdout)
+indent=$(python data/tools/wmlindent -d . 2>&1)
+printf "%s\n" "${indent[@]}" | tac
 
 if [[ -z $indent && -z $lint ]]; then
     exit 0
