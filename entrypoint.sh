49ff6a0628741b14bb938cad1a34a78716569de6
MJ <john.hervicc@gmail.com>
2022-01-07 11:48:12 +0100

    (#1) Improve logging.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,13 @@
-#!/bin/bash
+#!/bin/bash -l
 
-set -m
+# Capturing Python script stdout.
+exec 5>&1
 
 echo "Running wmllint."
-lint=$(python /data/wesnoth/data/tools/wmllint -d -S . 2>&1)
-printf "%s\n" "${lint[@]}" | tac
+lint=$(python /data/wesnoth/data/tools/wmllint -d -S . 2>&1 | tee >(cat - >&5))
 
 echo "Running wmlindent."
-indent=$(python /data/wesnoth/data/tools/wmlindent -d . 2>&1)
-printf "%s\n" "${indent[@]}" | tac
+indent=$(python /data/wesnoth/data/tools/wmlindent -d . 2>&1 | tee >(cat - >&5))
 
 if [[ -z $indent && -z $lint ]]; then
     exit 0
